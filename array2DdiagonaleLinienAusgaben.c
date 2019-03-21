dc8a9c14b280033f1593cc32a32d9774fc0c7c19
Dan Li <akiraryo@hotmail.com>
2019-03-21 13:46:16 +0100

    从左上角到右下角对角完成

--- array2DdiagonaleLinienAusgaben.c
+++ array2DdiagonaleLinienAusgaben.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 int main() {
+	int a[4][4] = { 1,1,1,1,
+					2,2,2,2,
+					3,3,3,3,
+					4,4,4,4,};
 
-
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == j)
+			{
+				int t = i;
+				while (t--)
+				{
+					printf("  ");
+				}
+					printf("%d", a[i][j]);		
+			}
+		}
+		putchar(10);
+	}
 }
